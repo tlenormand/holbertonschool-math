8dc26a9715dd14320dca67320492d11119e3528d
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-12 16:26:46 +0000

    part 8

--- 0x00-math_complex/8-complex.c
+++ 0x00-math_complex/8-complex.c
@@ -9,9 +9,6 @@
 
 void complex_from_mod_arg (double m, double arg, complex *c3)
 {
-	c3->re = (c1.re * c2.re + c1.im * -c2.im * -1);
-	c3->re /= (c2.re * c2.re + c2.im * -c2.im * -1);
-
-	c3->im = (c1.re * -c2.im + c1.im * c2.re);
-	c3->im /= (c2.re * c2.re + c2.im * -c2.im * -1);
+	c3->re = m * cos(arg);
+	c3->im = m * sin(arg);
 }
