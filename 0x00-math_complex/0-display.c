2c132d680cbc9cb83afc331bf6c9dda4f966ee0e
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-12 14:21:33 +0000

    correct display if c.im < 0

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -10,10 +10,10 @@ void display_complex_number(complex c)
 	printf("%g", c.re);
 
 /* print sign */
-	if (c.im < 0)
-		printf(" - ");
 	if (c.im > 0)
 		printf(" + ");
+	if (c.im < 0)
+		printf(" - "), c.im = -c.im;
 
 /* print im */
 	if (c.im != 0 && c.im != 1 && c.im != -1)
